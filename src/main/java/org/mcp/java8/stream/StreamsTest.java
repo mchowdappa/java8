13d66302b6bffc5d6a75c83711754944450e8a53
cm1 <chowdappa_m@intuit.com>
2017-11-27 21:13:57 +0530

    working on streams

--- src/main/java/org/mcp/java8/stream/StreamsTest.java
+++ src/main/java/org/mcp/java8/stream/StreamsTest.java
@@ -1,13 +1,22 @@
 package org.mcp.java8.stream;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class StreamsTest {
 
 	public static void main(String[] args) {
-	List<Integer> list = new ArrayList<>();
-	
+		// Stream Creation.
+
+		/**
+		 * 1. Creating Empty stream.
+		 * 2. Stream of Collections/List
+		 */
+		List<String> names = Collections.emptyList();
+		@SuppressWarnings("unused")
+		Stream<String> streamNames = (names == null || names.isEmpty()) ? Stream.empty() : names.stream();
+		
 	}
 
 }
